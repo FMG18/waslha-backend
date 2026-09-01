2bf077d940fac2de74b16ada0ef011541637c134
FMG18 <48799138+FMG18@users.noreply.github.com>
2026-09-01 18:03:17 +0300

    feat: add captain ratings route

---
+++ routes/captainRatings.js
@@ -0,0 +1,1 @@
+const express=require('express');const {auth,authorize}=require('../middleware/auth');const controller=require('../controllers/captainRatingController');const router=express.Router();router.use(auth,authorize('captain'));router.get('/',controller.list);module.exports=router;
